247bc6e4fdf93752a2f90be891560b310c27f26b
Aliona Sauchuk <aliona.sauchuk@LeverX.local>
2019-01-08 16:47:04 +0300

    task 1

---
+++ script1.js
@@ -0,0 +1,27 @@
+var newElement = document.createElement("p");
+var name =  prompt("Please enter your name", "Harry Potter")
+
+
+var myRe = /[0-9]/;
+var isContainNumber = myRe.test(name);
+if(isContainNumber === true){
+    var tempArr = name.split("");
+
+    for(var i=0; i< tempArr.length; i+=2){
+        tempArr[i] = tempArr[i].toString().toUpperCase();
+    }
+
+    for(var i=1; i< tempArr.length; i+=2){
+        tempArr[i] = tempArr[i].toString().toLowerCase();
+    }
+
+    name = tempArr.join('');
+}
+else{
+    name = name.split("").reverse().join("");
+}
+
+newElement.innerHTML = name;
+
+
+document.getElementById("userName").appendChild(newElement);
